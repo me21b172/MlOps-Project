62ba3484fcad66af29aab86c0feadede6fbe3d5e
Anuj Jagannath Said <me21b172@smail.iitm.ac.in>
2025-04-28 23:28:15 +0530

    Upading dvc.yaml second time

--- model/run.py
+++ model/run.py
@@ -7,11 +7,9 @@ import pandas as pd
 pipeline_process = None
 
 def run_mlflow_server():
-    """Thread target function for server"""
-    cmd = ["mlflow", "server", "--host", "0.0.0.0", "--port", "8080"]
-    process = subprocess.Popen(cmd)
-    atexit.register(process.terminate)
-    process.wait()  # Blocks until server terminates
+    """Start MLflow server in a new terminal window"""
+    cmd = ["start", "cmd", "/k", "mlflow server --host 0.0.0.0 --port 8080"]
+    subprocess.Popen(cmd, shell=True)
 
 def start_datapipeline():
     """Start the data pipeline process"""
@@ -33,13 +31,12 @@ def start_datapipeline():
 
 if __name__ == "__main__":
     # Start in dedicated thread
-    server_thread = Thread(target=run_mlflow_server, daemon=True)
-    server_thread.start()
+    run_mlflow_server()
     time.sleep(5)  # Give server time to start
 
     # Start the data pipeline in a separate thread
     while(True):
         data = pd.read_csv("data/news_feed.csv")
-        if len(data) > 10:
+        if len(data) > 2:
             start_datapipeline()
-        time.sleep(600)
+        time.sleep(400)
